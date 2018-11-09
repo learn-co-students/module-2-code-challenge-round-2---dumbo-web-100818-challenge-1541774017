db5f1a57f15706114d50f3e1319c9db8a8eaa96d
github username <github email address>
2018-11-09 11:04:43 -0500

    I irreversibly messed up the first time, so I created this branch with the good stuff

---
+++ app/controllers/appearances_controller.rb
@@ -0,0 +1,24 @@
+class AppearancesController < ApplicationController
+  def new
+    @appearance = Appearance.new
+  end
+
+  def create
+    @appearance = Appearance.new(appearance_params)
+    if @appearance.save
+      redirect_to episode_path(@appearance.episode)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @appearance = Appearance.find(params[:id])
+  end
+
+  private
+
+  def appearance_params
+    params.require(:appearance).permit(:episode_id, :guest_id, :rating)
+  end
+end
