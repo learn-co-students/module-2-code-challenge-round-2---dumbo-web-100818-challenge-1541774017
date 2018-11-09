735d356d75521cce1f78b2a3993ce3e37a7800c4
github username <github email address>
2018-11-09 11:15:42 -0500

    t help

---
+++ app/controllers/appearances_controller.rb
@@ -0,0 +1,22 @@
+class AppearancesController < ApplicationController
+	def new
+		@appearance = Appearance.new
+    @guest = Guest.all
+    @episode = Episode.all
+	end
+
+	def create
+		@appearance = Appearance.find_or_create_by(appearance_params)
+		if @appearance.valid?
+			redirect_to @appearance.episode
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def appearance_params
+		params.require(:appearance).permit(:episode_id, :guest_id, :rating)
+	end
+end
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,4 @@
 class Episode < ApplicationRecord
+  has_many :apperances
+  has_many :guest, through: :apperances
 end
