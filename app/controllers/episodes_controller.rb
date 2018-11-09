737299b1ae3ea5480c2071c4abcb7f270085f23d
github username <github email address>
2018-11-09 11:12:56 -0500

    Thanks

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -3,4 +3,11 @@ class EpisodesController < ApplicationController
   def index
     @episodes = Episode.all
   end
+
+  def show
+   @episode = Episode.find(params[:id])
+   @guests = @episode.guests
+  end
+
+  
 end
